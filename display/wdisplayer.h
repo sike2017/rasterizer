e2ff21a32932c758f5d521fdb10c7838d505b96b
szhn <sikegrbd@outlook.com>
2020-12-18 14:46:28 +0800

    添加项目文件。

---
+++ display/wdisplayer.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <Windows.h>
+#include "painter.h"
+
+extern Painter painter;
+
+// do not using this namespace in your program!
+namespace display
+{
+	LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+}
+
+class WindowDisplayer
+{
+public:
+	WindowDisplayer(HINSTANCE thInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow, int twidth, int theight, const TCHAR* twindowName);
+	~WindowDisplayer() {}
+
+	int display();
+	void setPixel(int x, int y, RGBA color);
+	void update();
+	
+	int width() const;
+	int height() const;
+
+protected:
+	int colorIndex;
+
+	int _width, _height;
+
+	enum class RENDER_STATUS
+	{
+		CALL_NEXTTIME,
+		CALL_STOP,
+		CALL_STOP_SAVE_IMAGE
+	};
+
+	RENDER_STATUS renderStatus;
+
+	virtual RENDER_STATUS render() {
+		RGBA color0(0, 162, 232, 0);
+		RGBA color1(112, 146, 190, 0);
+
+		int width = 400;
+		int height = 300;
+		int size = width * height;
+		for (int i = 0; i < size; i++)
+		{
+			int x = i % width;
+			int y = i / width;
+			if (!colorIndex)
+			{
+				setPixel(x, y, color0);
+			}
+			else {
+				setPixel(x, y, color1);
+			}
+		}
+
+		colorIndex = !colorIndex;
+
+		update();
+		return RENDER_STATUS::CALL_NEXTTIME;
+	}
+
+private:
+	HINSTANCE hInstance;
+	HINSTANCE hPrevInstance;
+	LPSTR szCmdLine;
+	int iCmdShow;
+	const TCHAR* windowName;
+	
+	RenderBitmap rb;
+
+	int InitWindow();
+};
---
+++ rasterizer.cpp
@@ -0,0 +1,11 @@
+﻿#include "rasterizer.h"
+#include "log/log.h"
+#include "display/wdisplayer.h"
+
+using namespace std;
+
+int main()
+{
+	rlog << "Hello CMake.\n" << rendl;
+	return 0;
+}
